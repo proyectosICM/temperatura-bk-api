e139078fe7bd367b19c965d8c057cbd3288454dd
Eduardo Aguilar <es123y123@gmail.com>
2025-08-12 12:45:28 -0500

    c

--- src/main/java/com/icm/temperatura_bk_api/controllers/TemperatureLogController.java
+++ src/main/java/com/icm/temperatura_bk_api/controllers/TemperatureLogController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,7 +37,7 @@ public class TemperatureLogController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
         Page<TemperatureLogModel> logs = temperatureLogService.findByPlatformId(platformId, pageable);
         return ResponseEntity.ok(logs);
     }
